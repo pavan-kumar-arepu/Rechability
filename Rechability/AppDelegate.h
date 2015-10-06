104393d6a37f53f736b584e4406052d68fd07140
Arepu <iosdeveloper.ipa@gmail.com>
2015-10-06 23:51:45 +0530

    Initial Commit

---
+++ Rechability/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Rechability
+//
+//  Created by Pavankumar Arepu on 06/10/15.
+//  Copyright (c) 2015 PPAM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

6693883b9ce5fe3048fa62d82dd99ec8501a9061
Arepu PavanKumar <iosdeveloper.ipa@gmail.com>
2015-10-15 19:48:17 +0530

    Initial Commit on Reachability Class
    Initial Commit on Reachability Class

--- Rechability/ViewController.h
+++ Rechability/ViewController.h
@@ -8,8 +8,9 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<UIAlertViewDelegate>
 
+@property (weak, nonatomic) IBOutlet UILabel *myLabel;
 
 @end
 
--- Rechability/ViewController.m
+++ Rechability/ViewController.m
@@ -7,19 +7,36 @@
 //
 
 #import "ViewController.h"
-
+#import "Reachability.h"
 @interface ViewController ()
 
 @end
 
 @implementation ViewController
-
 - (void)viewDidLoad {
     [super viewDidLoad];
+    Reachability *networkReachability = [Reachability reachabilityForInternetConnection];
+    NetworkStatus networkStatus = [networkReachability currentReachabilityStatus];
+    if (networkStatus == NotReachable)
+    {
+        self.view.backgroundColor = [UIColor redColor];
+        _myLabel.text = @"Wifi Not Avaialable";
+        UIAlertView *alert=[[UIAlertView alloc]initWithTitle:@"Internet Connection"
+                                                     message:@"Not Available :-(" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+        [alert show];
+    }
+    else
+    {
+        _myLabel.text = @"Wifi Avaialable";
+        self.view.backgroundColor = [UIColor greenColor];
+        UIAlertView *alert=[[UIAlertView alloc]initWithTitle:@"Internet Connection"
+                                                     message:@"Available :-) " delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+        [alert show];
+    }
     // Do any additional setup after loading the view, typically from a nib.
 }
-
-- (void)didReceiveMemoryWarning {
+- (void)didReceiveMemoryWarning
+{
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
